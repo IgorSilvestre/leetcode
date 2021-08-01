09af961f410051e18c0613de4fdd9ef6337fb583
IgorSilvestre <igormorais.silvestre@gmail.com>
2021-07-31 23:03:41 -0300

    fixing buggy adress error in duplicatezeros.cpp file

--- arrays101/duplicateZeros.cpp
+++ arrays101/duplicateZeros.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std;
-
-int main (){
-  int arr[] = {1, 0, 2, 3, 0, 4, 5, 0};
-  for (int i = 0; i < arr.size(); i ++){
-    if (arr[i] == 0){
-      if (arr[i - 1] == 0){
-        continue;
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+      for (int i = 0; i < arr.size(); i ++){
+        if (arr[i] == 0){
+          for (int j = arr.size() - 2; j >= i; j --){
+            arr[j + 1] = arr[j];
+          }
+          i++;
+        }
       }
-      arr[i + 1] = 0;
+
     }
-  }
-}
+};
